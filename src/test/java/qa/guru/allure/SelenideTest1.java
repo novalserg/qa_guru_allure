1203073488019d3a21c357160d7d26884b01f4e7
Alexandra Zabnenkova <novalserg@yandex.com>
2025-10-04 18:30:51 +0300

    first

---
+++ src/test/java/qa/guru/allure/SelenideTest1.java
@@ -0,0 +1,28 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest1 {
+
+    @Test
+    public void IssueSearchTest(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com/");
+
+        $("##repositories-tab").click();
+        $(By.linkText("QA_GURU_JUnit_HW")).click();
+
+        $("#issue-tab").click();
+        $(By.linkText("/novalserg/QA_GURU_JUnit_HW/issues/new/choose")).click();
+        $("#issue-create-pane-title").shouldBe(Condition.visible);
+
+    }
+}
